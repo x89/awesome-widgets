ff1c9846e5bae595f9aeee467f18015b194968ff
David Trail <napalm@trailbeans.eu>
2014-08-10 13:38:03 +0100

    We don't want to startx in ~/.config/awesome

--- orangered/orangered.lua
+++ orangered/orangered.lua
@@ -1,11 +1,15 @@
+local awful = require("awful")
+
+-- {{{ Orangered Widget helper
 function get_orangereds()
-	local file = io.popen('python check_mail.py')
+	local file = io.popen('python ' .. awful.util.getdir("config") .. 'check_mail.py')
 	local output = file:read('*all')
 	local rc = {file:close()}
 
 	if rc[3] > 0 then
-		return "" .. rc[3] .. " orangereds"
+		return awful.util.getdir("config") .. "/images/unread.png"
 	else
-		return ""
+		return awful.util.getdir("config") .. "/images/read.png"
 	end
 end
+--- }}}
